89aee789125a62eab580c1c40a0cc21f6b26a65b
Gustavo Marques | Plano Angelus <gustavo.marques@planoangelus.com.br>
2026-04-27 14:34:44 -0300

    :sparkles: feat: adição de estrutura de pasta para guardar resultados

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+import os
 import torch
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
@@ -7,7 +8,9 @@ import torch.optim as optim
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
 import seaborn as sns
-import numpy as np
+
+# garantir pasta de saída
+os.makedirs("outputs", exist_ok=True)
 
 # transformação
 transform = transforms.Compose([
@@ -15,7 +18,8 @@ transform = transforms.Compose([
     transforms.RandomHorizontalFlip(),
     transforms.RandomRotation(15),
     transforms.ColorJitter(brightness=0.2, contrast=0.2),
-    transforms.ToTensor()
+    transforms.ToTensor(),
+    transforms.Normalize([0.5], [0.5])
 ])
 
 # dataset
@@ -57,7 +61,7 @@ for epoch in range(20):
     train_acc = correct / total
     train_acc_list.append(train_acc)
 
-# validação
+    # validação
     model.eval()
     correct = 0
     total = 0
@@ -75,10 +79,10 @@ for epoch in range(20):
 
     print(f"Epoch {epoch+1} | Train Acc: {train_acc:.2f} | Val Acc: {val_acc:.2f}")
 
-# salvar melhor modelo
+    # salvar melhor modelo
     if val_acc > best_val_acc:
         best_val_acc = val_acc
-        torch.save(model.state_dict(), "best_model.pth")
+        torch.save(model.state_dict(), "outputs/best_model.pth")
 
 # gráfico
 plt.plot(train_acc_list, label="Train Accuracy")
@@ -89,9 +93,10 @@ plt.ylabel("Accuracy")
 plt.title("Training vs Validation Accuracy")
 plt.legend()
 
-plt.savefig("training_graph.png")
+plt.savefig("outputs/training_graph.png")
 plt.show()
 
+# confusion matrix
 model.eval()
 
 all_preds = []
@@ -105,12 +110,12 @@ with torch.no_grad():
         all_preds.extend(preds.cpu().numpy())
         all_labels.extend(labels.cpu().numpy())
 
-cm = confusion_matrix(all_labels, all_preds)
+cm = confusion_matrix(all_labels, all_preds, normalize='true')
 
 class_names = train_data.classes
 
 plt.figure(figsize=(6,5))
-sns.heatmap(cm, annot=True, fmt='d', cmap='Blues',
+sns.heatmap(cm, annot=True, fmt='.2f', cmap='Blues',
             xticklabels=class_names,
             yticklabels=class_names)
 
@@ -118,5 +123,5 @@ plt.xlabel("Predicted")
 plt.ylabel("True")
 plt.title("Confusion Matrix")
 
-plt.savefig("confusion_matrix.png")
+plt.savefig("outputs/confusion_matrix.png")
 plt.show()
